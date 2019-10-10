cc815d771f18d88f150adb0992cefb8b697f5fb7
Adam Delaney <44587502+adelaney11@users.noreply.github.com>
2019-10-10 11:32:06 -0500

    Add puzzle reading files
    An example puzzle txt file and how it should be formatted, along with the Python function to read the txt file input

---
+++ txtfile_read.py
@@ -0,0 +1,14 @@
+def readPuzzle(file):
+    with open(file) as my_file:
+       puzzle = {}
+       for line in my_file:
+           if line.isupper():
+               title = line
+               puzzle[title] = []
+           if line != '\n' and line != title:
+               puzzle[title].append(line)
+           else:
+               continue 
+    return(puzzle)
+
+print(readPuzzle("puzzle.txt"))
